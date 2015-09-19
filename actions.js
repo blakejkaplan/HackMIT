b83c97e844399a2786a6540898b7750cb166fd89
blakejkaplan <blakejkaplan@gmail.com>
2015-09-19 14:21:06 -0400

    Rough model for a site

---
+++ actions.js
@@ -0,0 +1,12 @@
+$(document).ready(
+	function(){
+	$("#button1").click(
+		function (){
+			alert("This worked");
+		});
+	$("button").click(
+		function(){
+			alert("This is different");
+		});
+		}
+	);
